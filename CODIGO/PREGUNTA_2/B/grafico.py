48f1a5286e9c0295b1cc498f3eb8d9c5d906badc
LukasWolff2002 <lwolff@miuandes.cl>
2024-09-22 12:02:53 -0300

    avance

---
+++ CODIGO/PREGUNTA_2/B/grafico.py
@@ -0,0 +1,29 @@
+from kt_segun_distribucion import df
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(19.2, 10.8))  # Tamaño en pulgadas que corresponde a 1920x1080 píxeles
+plt.rcParams.update({'font.size': 16})  # Ajusta el tamaño base de la fuente
+plt.subplots_adjust(left=0.05, right=0.95, top=0.95, bottom=0.09)
+
+def graficar_columnas(df, columna_x, columna_y, color):
+
+    plt.plot(df[columna_x], df[columna_y], marker='o', linestyle='-', color=color)
+
+    
+
+graficar_columnas(df, 'ACCESS1-0 [r1i1p1]', 'Kt Normal', 'red')
+graficar_columnas(df, 'ACCESS1-0 [r1i1p1]', 'Kt Log-Normal', 'green')
+graficar_columnas(df, 'ACCESS1-0 [r1i1p1]', 'Kt Pearson III', 'blue')
+graficar_columnas(df, 'ACCESS1-0 [r1i1p1]', 'Kt Gumbel', 'black')
+# Título del gráfico
+plt.title(f'Gráfico de',fontsize=25)
+plt.legend(['Normal', 'Log-Normal', 'Pearson III', 'Gumbel'], fontsize=18)
+# Etiquetas de los ejes
+plt.xlabel("Precipitacion", fontsize=18)
+plt.ylabel("Frecuencia segun distribucion", fontsize=18)
+
+# Guardar el gráfico como una imagen JPG con la resolución deseada
+plt.savefig('grafico_b.jpg', format='jpg', dpi=100)
+
+# Cerrar la figura para liberar memoria
+plt.close()
---
+++ CODIGO/PREGUNTA_2/B/lectura_xlsx.py
@@ -0,0 +1,19 @@
+#Primero debo leer el archivo
+import pandas as pd
+
+def leer_hoja_excel(archivo):
+
+    try:
+        # Leer la hoja especificada del archivo Excel
+        df = pd.read_excel(archivo, engine='openpyxl')
+        return df
+    except Exception as e:
+        print(f"Ocurrió un error al leer el archivo: {e}")
+        return None
+
+# Ejemplo de uso
+archivo = 'DATOS/P_B.xlsx'
+
+df_precipitaciones = leer_hoja_excel(archivo)
+
+print(df_precipitaciones)
